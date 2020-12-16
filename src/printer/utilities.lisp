6f1c03b5e9dd8682ee2affc166196e632e043c48
Robert Strandh <robert.strandh@gmail.com>
2020-12-16 16:41:53 +0100

    Temporarily disable macro.

--- src/printer/utilities.lisp
+++ src/printer/utilities.lisp
@@ -5,9 +5,9 @@
   (print-object-using-client client (type-of obj) stream)
   (write-char #\>))
 
-(defmacro def-identical-printers ((var (&rest types)) &body body)
-  `(progn
-     ,@(loop :for type :in types
-             :collect `(defmethod print-object-using-client ((client standard-client)
-                                                             (,var ,type) stream)
-                         ,@body))))
+;; (defmacro def-identical-printers ((var (&rest types)) &body body)
+;;   `(progn
+;;      ,@(loop :for type :in types
+;;              :collect `(defmethod print-object-using-client ((client standard-client)
+;;                                                              (,var ,type) stream)
+;;                          ,@body))))
