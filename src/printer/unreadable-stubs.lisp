b6aae86d9914f303ed8ca055a1da1c980dd803c7
Robert Strandh <robert.strandh@gmail.com>
2020-12-16 16:42:13 +0100

    Temporarily disable macro call.

--- src/printer/unreadable-stubs.lisp
+++ src/printer/unreadable-stubs.lisp
@@ -1,7 +1,7 @@
 (cl:in-package #:incless)
 
 ;;; none of these are required to be printed readably by the spec.
-(def-identical-printers (foo (hash-table readtable package stream function))
-  (if *print-readably*
-      (error 'print-not-readable :object foo)
-      (unreadably client foo stream)))
+;; (def-identical-printers (foo (hash-table readtable package stream function))
+;;   (if *print-readably*
+;;       (error 'print-not-readable :object foo)
+;;       (unreadably client foo stream)))
