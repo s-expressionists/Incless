0e22b1b37dd482ad7d84f5aca726d546d7b83038
Robert Strandh <robert.strandh@gmail.com>
2020-12-17 15:28:00 +0100

    Define macro PRINT-UNREADABLE-OBJECT.

--- src/printer/utilities.lisp
+++ src/printer/utilities.lisp
@@ -5,6 +5,28 @@
   (print-object-using-client client (type-of obj) stream)
   (write-char #\>))
 
+;;; FIXME figure out how to print an identity
+(defmacro print-unreadable-object
+    ((object stream &key type identity) &body body)
+  (let ((object-var (gensym))
+        (stream-var (gensym))
+        (type-var (gensym))
+        (identity-var (gensym)))
+    `(let ((,object-var ,object)
+           (,stream-var ,stream)
+           (,type-var ,type)
+           (,identity-var ,identity))
+       (if *print-readably*
+           (error 'print-not-readable :object ,object-var)
+           (progn
+             (write-string  "#<" ,stream-var)
+             (when ,type-var
+               (write-string (symbol-name (class-name (class-of ,object-var))) ,stream-var))
+             (when ,identity-var
+               (write-string  " ID" ,stream-var))
+             ,@body
+             (write-string ">" ,stream-var))))))
+
 ;; (defmacro def-identical-printers ((var (&rest types)) &body body)
 ;;   `(progn
 ;;      ,@(loop :for type :in types
