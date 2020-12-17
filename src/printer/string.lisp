02730b7d90c554ba5b016ae61754e19e73b7bdf3
Robert Strandh <robert.strandh@gmail.com>
2020-12-17 16:35:41 +0100

    Simplify by not creating an intermediate string with escape characters.

--- src/printer/string.lisp
+++ src/printer/string.lisp
@@ -1,15 +1,12 @@
 (in-package #:incless)
 
-(defun escape-string (string)
-  (with-output-to-string (stream)
-    (write-char #\" stream)
-    (loop :for x :across string
-          :do (when (or (char= x #\") (char= x #\\))
-                (write-char #\\ stream))
-              (write-char x stream))
-    (write-char #\" stream)))
-
-(defmethod print-object-using-client ((client standard-client) (obj string) stream)
+(defmethod print-object-using-client
+    ((client standard-client) (object string) stream)
   (if (or *print-escape* *print-readably*)
-      (write-string (escape-string obj) stream)
+      (progn (write-char #\" stream)
+             (loop :for x :across object
+                   :do (when (or (char= x #\") (char= x #\\))
+                         (write-char #\\ stream))
+                       (write-char x stream))
+             (write-char #\" stream))
       (write-string obj stream)))
