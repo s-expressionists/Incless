91939968ca3f05c41cbd85ce8260dde805013440
Birk Hirdman <lonjil@gmail.com>
2020-12-18 22:58:49 +0100

    Print symbols better, still incomplete.

--- src/printer/symbol.lisp
+++ src/printer/symbol.lisp
@@ -3,4 +3,12 @@
 ;;; placeholder implementation needed by other printers
 
 (defmethod print-object-using-client ((client standard-client) (sym symbol) stream)
-  (write-string (symbol-name sym)))
+  (let ((package (symbol-package sym)))
+    (cond ((null package)
+           (write-string "#:" stream))
+          ((eq package (find-package "KEYWORD")) ; seems inefficient
+           (write-string ":" stream))
+          ((not (eq package *package*))
+           (write-string (package-name package) stream)
+           (write-string ":" stream)))
+    (write-string (symbol-name sym) stream)))
