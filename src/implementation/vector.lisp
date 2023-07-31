4d3021c0e05037493fff99183b9606b1e070bfc8
Tarn W. Burton <twburton@gmail.com>
2023-07-31 06:35:34 -0400

    Add readable array printing to print-vector

--- src/implementation/vector.lisp
+++ src/implementation/vector.lisp
@@ -1,24 +1,48 @@
 (cl:in-package #:incless-implementation)
 
 (defun print-vector (client vec stream)
-  (cond ((and (not *print-readably*)
-              (eql 0 *print-level*))
-         (write-char #\# stream))
-        ((or *print-array* *print-readably*)
-         (write-string "#(" stream)
-         (loop with *print-level* = (and *print-level* (max 0 (1- *print-level*)))
-               for item across vec
-               for index from 0
-               unless (zerop index)
-                 do (write-char #\Space stream)
-               if (or (null *print-length*)
-                      *print-readably*
-                      (< index *print-length*))
-                 do (incless:write-object client item stream)
-               else
-                 do (write-string "..." stream)
-                    (loop-finish))
-         (write-string ")" stream))
-        (t
-         (incless:write-unreadable-object client vec stream t t nil)))
-  vec)
+  (flet ((print-guts (openp)
+           (when openp
+             (write-char #\( stream))
+           (loop with *print-level* = (and *print-level* (max 0 (1- *print-level*)))
+                 for item across vec
+                 for index from 0
+                 unless (zerop index)
+                   do (write-char #\Space stream)
+                 if (or (null *print-length*)
+                        *print-readably*
+                        (< index *print-length*))
+                   do (incless:write-object client item stream)
+                 else
+                   do (write-string "..." stream)
+                      (loop-finish))
+           (write-char #\) stream)))
+    (cond ((and (not *print-readably*)
+                (eql 0 *print-level*))
+           (write-char #\# stream))
+          #+(or clasp clisp cmucl ecl mkcl)
+          ((and *print-readably*
+                (or (not (eq t (array-element-type vec)))
+                    (some #'zerop (array-dimensions vec))))
+           (write-string "#A(" stream)
+           (incless:write-object client (array-element-type vec) stream)
+           (write-char #\Space stream)
+           (incless:write-object client (array-dimensions vec) stream)
+           (write-char #\Space stream)
+           (print-guts t)
+           (write-char #\) stream))
+          #+sbcl
+          ((and *print-readably*
+                (or (not (eq t (array-element-type vec)))
+                    (some #'zerop (array-dimensions vec))))
+           (write-string "#A(" stream)
+           (incless:write-object client (array-dimensions vec) stream)
+           (write-char #\Space stream)
+           (incless:write-object client (array-element-type vec) stream)
+           (write-char #\Space stream)
+           (print-guts nil))
+          ((or *print-array* *print-readably*)
+           (write-char #\# stream)
+           (print-guts t))
+          (t
+           (incless:write-unreadable-object client vec stream t t nil)))))
