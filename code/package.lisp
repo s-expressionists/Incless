e8ed322327c2d856355a99b11a8d3714ee7d776c
Tarn W. Burton <twburton@gmail.com>
2026-04-06 10:07:46 -0400

    Account for deleted packages

--- code/package.lisp
+++ code/package.lisp
@@ -1,6 +1,9 @@
 (in-package #:incless)
 
 (defun print-package (client package stream)
-  (write-unreadable-object client package stream t nil
-                           (lambda ()
-                             (print-string client (package-name package) stream))))
+  (let ((name (package-name package)))
+    (write-unreadable-object client package stream t (not name)
+                             (lambda ()
+                               (if name
+                                   (print-string client name stream)
+                                   (write-string "(deleted)" stream))))))
